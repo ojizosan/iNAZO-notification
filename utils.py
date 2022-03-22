5095db7dd5263ad4ed904e56c9d7af707fa9c1c3
ojizosan <ojizosan@users.noreply.github.com>
2022-03-22 14:20:28 +0900

    Add get_html and get_terms func

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
-import bs4
+import re
 import requests
+from bs4 import BeautifulSoup
 
 def get_html(url):
-    r = requests.get(url)
-    return r.text
+    res = requests.get(url)
+    return res.text
 
+def get_terms(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    options = [o.text for o in soup.find('select', id='ddlTerm').find_all('option') if o.text != '---------------']
+    
+    results = [tuple(map(int, re.findall(r'\d+', o))) for o in options]
+    return results
